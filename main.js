9fae432efe0110b0bdc6e3bd17c69ece1524e47c
Lola Gasanova <63504067+GasanovaLola@users.noreply.github.com>
2020-10-17 18:46:21 +0300

    commit first

---
+++ main.js
@@ -0,0 +1,45 @@
+// 1
+
+function funk(n) {
+    console.log(n);
+
+    if (n !== 1) {
+        funk(n-1);
+    }
+    else {
+        return true;
+    }
+}
+
+funk(5);
+
+// 2
+
+function sumTo(n) {
+    if (n === 1) {
+        return n;
+    }
+    return n + sumTo(n-1);
+}
+
+console.log(sumTo(3));
+
+// 3
+
+function factorial(n) {
+    if (n === 1) {
+        return n;
+    }
+    return n * factorial(n - 1);
+}
+
+console.log(factorial(4));
+
+// 4
+
+mass = [[1,2,3, [3.1]], 4, [5,6, [7, 8]]];
+
+function funkConvert(mass) {
+   return mass.reduce((acc, val) => Array.isArray(val) ? acc.concat(funkConvert(val)) : acc.concat(val), []);
+}
+console.log(funkConvert(mass));
